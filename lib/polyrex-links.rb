988095a2040a5fc3df1b90b2e883dccf26d983ff
James Robertson <james@r0bertson.co.uk>
2018-04-12 16:57:51 +0100

    feature: Implemented to_h() and link() which returns the link as a Polyrex object.

--- lib/polyrex-links.rb
+++ lib/polyrex-links.rb
@@ -8,10 +8,20 @@ class PolyrexLinks < Polyrex
 
   def initialize(x='links/link[name,url]', delimiter: ' # ', debug: false)
     super(x)
-    self.delimiter = delimiter
+    @delimiter = self.delimiter = delimiter
     @debug = debug
   end
-
+  
+  # Returns the Polyrex element for the given path
+  #
+  def link(s)
+    
+    self.rxpath(s.split('/').map {|x| "link[name='%s']" % x}.join('/')).first
+    
+  end
+  
+  # Returns the url and remaining arguments for the given path
+  #
   def locate(raw_path)
     
     path = raw_path.sub(/^\//,'')
@@ -30,8 +40,38 @@ class PolyrexLinks < Polyrex
     #   it didn't find, or it will return nil.
     
     puts "c: %s\npath: %s\nr: %s" % [c, path, r].map(&:inspect) if @debug
-    
+
     r ? [r, path.sub(c.join('/'),'')] : nil
   end
+  
+  
+  # Return a flat Hash object containing all the links. The link path is 
+  # used as the key and the value is the url
+  #
+  def to_h()
+    
+    h = {}
+    
+    each_link {|key, value| h[key] = value }
+    h
+    
+  end
+  
+  private
+  
+
+  def each_link()
+
+    a = []
+    
+    self.each_recursive() do |record, _, i|       
+      
+        a.slice!(i..-1)
+        a[i] = record
+        yield(a.map(&:name).join('/'), record.url) if block_given?
+        
+    end
+             
+  end  
 
 end
